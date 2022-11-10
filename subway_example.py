1a5d9acca3333f2c5aaf959d3802f00035bfcd8e
Aryeh Zapinsky <aryeh.zapinsky@columbia.edu>
2022-11-09 22:16:14 -0500

    Initial commit

---
+++ subway_example.py
@@ -0,0 +1,52 @@
+
+from google.transit import gtfs_realtime_pb2
+import urllib.request
+from datetime import datetime
+from datetime import timedelta
+
+def minutes_until_next_train(arrival_time, now):
+  return int((arrival_time - now).total_seconds()/60)
+
+def get_upcoming_trains():
+  subway_endpoint = "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace"
+  api_key = "EQLPiOQ7xF5ZvuShu0hgT3sBpBwimCeh8rqN1IiF"
+  request = urllib.request.Request(subway_endpoint)
+  request.add_header('x-api-key', api_key)
+
+  response = urllib.request.urlopen(request)
+
+  now = datetime.now()
+  upcoming_trains = []
+
+  feed = gtfs_realtime_pb2.FeedMessage()
+  feed.ParseFromString(response.read())
+  for entity in feed.entity:
+    if entity.HasField('trip_update'):
+      if entity.trip_update.HasField("trip"):
+          if (entity.trip_update.trip.HasField("route_id")) and (entity.trip_update.trip.HasField("trip_id")):
+            trip = entity.trip_update.trip
+            if (trip.route_id == "A") and (trip.trip_id[-1] == "S"):
+              # print(trip)
+              for stop_time_update in entity.trip_update.stop_time_update:
+                # 181st going south - A06S,,181 St,,40.851695,-73.937969,,,0,A06
+                # https://openmobilitydata-data.s3-us-west-1.amazonaws.com/public/feeds/mta/79/20220615/original/stops.txt
+                # https://transitfeeds.com/p/mta/79/latest
+                if stop_time_update.stop_id == "A06S":
+                  arriving = datetime.fromtimestamp(stop_time_update.arrival.time)
+                  # departing = datetime.fromtimestamp(stop_time_update.departure.time)
+                  # print("Arriving: {}, Departing: {}".format(arriving, departing))
+                  upcoming_trains.append(minutes_until_next_train(arriving, now))
+                  print("Next train arriving in {} minutes.".format(upcoming_trains[-1]))
+  
+  return upcoming_trains
+
+def filter_trains_next_two(upcoming_trains):
+  next_two = []
+  for train_time in upcoming_trains:
+    if (train_time == 0):
+      continue
+    next_two.append(train_time)
+    if (len(next_two) == 2):
+      return next_two
+
+print(filter_trains_next_two(get_upcoming_trains()))
---
+++ weather_example.py
@@ -0,0 +1,19 @@
+import urllib.request
+import json
+from datetime import time
+
+
+def get_weather():
+    weather_endpoint = "https://api.openweathermap.org/data/3.0/onecall"
+    weather_endpoint += "?lat=40.851800&lon=-73.937230"
+    weather_endpoint += "&appid=6a8aa4c20bb42649555d37ebf2c335a9"
+    weather_endpoint += "&units=imperial"
+    weather_endpoint += "&exclude=minutely,daily"
+    request = urllib.request.Request(weather_endpoint)
+    response = urllib.request.urlopen(request)
+    weather = json.load(response)
+    current_temp = round(weather["current"]["temp"])
+    temps_over_hours = [(round(t["temp"]), t["dt"]) for t in weather["hourly"]]
+    return temps_over_hours
+
+print(get_weather())
