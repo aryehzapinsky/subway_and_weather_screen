c8e6aa272d59f45a9444c54d317f1ce00550e198
Aryeh Zapinsky <aryeh.zapinsky@columbia.edu>
2022-11-28 21:57:08 -0500

    update UI

--- screen_example.py
+++ screen_example.py
@@ -21,15 +21,15 @@ class RunText(SampleBase):
         font_large.LoadFont("/home/aryeh/rpi-rgb-led-matrix/fonts/6x10.bdf")
         font_small = graphics.Font()
         font_small.LoadFont("/home/aryeh/rpi-rgb-led-matrix/fonts/5x8.bdf")
-        textColor = graphics.Color(52,85,235)
+        textColor = graphics.Color(207,207,207)
 
         while True:
             train_times = filter_trains_next_two(get_upcoming_trains())
             
             offscreen_canvas.Clear()
-            graphics.DrawText(offscreen_canvas, font_small, 0, 10, textColor, "Train times:")
-            graphics.DrawText(offscreen_canvas, font_small, 0, 25, textColor, "1. {} minutes".format(train_times[0]))
-            graphics.DrawText(offscreen_canvas, font_small, 0, 40, textColor, "2. {} minutes".format(train_times[1]))
+            graphics.DrawText(offscreen_canvas, font_small, 0, 10, textColor, "TRAINS:")
+            graphics.DrawText(offscreen_canvas, font_large, 0, 10, textColor, "{} MIN".format(train_times[0]))
+            graphics.DrawText(offscreen_canvas, font_large, 0, 20, textColor, "{} MIN".format(train_times[1]))
             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
             current_hour = datetime.now().hour
