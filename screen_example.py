101e91b98f11e3b761ee97493e95aaa694c3b67e
Aryeh Zapinsky <aryeh.zapinsky@columbia.edu>
2022-11-28 21:39:21 -0500

    update fonts

--- screen_example.py
+++ screen_example.py
@@ -13,22 +13,22 @@ class RunText(SampleBase):
     def run(self):
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font_large = graphics.Font()
-        font_large.LoadFont("/home/aryeh/rpi-rgb-led-matrix/fonts/7x13.bdf")
+        font_large.LoadFont("/home/aryeh/rpi-rgb-led-matrix/fonts/6x9.bdf")
         font_small = graphics.Font()
-        font_small.LoadFont("/home/aryeh/rpi-rgb-led-matrix/fonts/5x8.bdf")
+        font_small.LoadFont("/home/aryeh/rpi-rgb-led-matrix/fonts/4x6.bdf")
         textColor = graphics.Color(52,85,235)
 
         while True:
             train_times = filter_trains_next_two(get_upcoming_trains())
             
             offscreen_canvas.Clear()
-            graphics.DrawText(offscreen_canvas, font_large, 0, 10, textColor, "Train times:")
+            graphics.DrawText(offscreen_canvas, font_small, 0, 10, textColor, "Train times:")
             graphics.DrawText(offscreen_canvas, font_small, 0, 25, textColor, "1. {} minutes".format(train_times[0]))
             graphics.DrawText(offscreen_canvas, font_small, 0, 40, textColor, "2. {} minutes".format(train_times[1]))
             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
             current_hour = datetime.now().hour
-            if (current_hour > 6 and current_hour < 21):
+            if (current_hour > 6 and current_hour < 23):
                 time.sleep(90)
             else:
                 time.sleep(60*30)
